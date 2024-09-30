133f5d5dac9635eb3039a476e693485bd9edd989
marcellypereira <marcellyp120@gmail.com>
2024-09-30 16:00:41 -0300

    :tada: Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.(js,jsx,ts,tsx)"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6347F9",
+        light: "#FFFFFF1A",
+        gray: "#9DA1B4",
+        lightGray: "#F4F6FA",
+        orange: "#FA9556",
+      }
+    },
+  },
+  plugins: [],
+}
+
